ad46206555f75e9fd1355c60b34efa7974ba346f
TnhatY <theanh0343@gmailcom>
2024-09-15 10:37:05 +0700

    source code

---
+++ Week_01/src/main/java/controller/hellor/HelloServlet.java
@@ -0,0 +1,49 @@
+package controller.hellor;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet(urlPatterns = { "/hello" })
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		 response.setContentType("text/html");
+	      response.setCharacterEncoding("UTF-8");
+	      request.setCharacterEncoding("UTF-8"); 
+		String ten = request.getParameter("ten");
+		 String holot = request.getParameter("holot");
+		 //Hiển thị dữ liệu ra web bằng đối tượng PrintWriter
+		 PrintWriter out = response.getWriter();
+		 out.println("<h1>Hello : " + holot + " "+ ten +"</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
